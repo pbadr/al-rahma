8372a30549cc5d6eeeb75fc7b384f2ea04c5c89f
Badr <badrkmc@gmail.com>
2023-11-01 21:26:55 +0500

    init requirements for model

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,20 @@
-from quart import Quart
+from quart import Quart, request
 
 app = Quart(__name__)
 
-@app.route('/')
+@app.route('/api', methods=['POST'])
 async def api():
+  data = await request.get_json()
+
+  if not "prompt" in data:
     return {
-      'message': 'Hello from the backend'
-    }, 200
+      'message': 'Error: No prompt provided'
+    }, 400
+
+  prompt = data['prompt']
+
+  return {
+    'message': 'Hello from the backend'
+  }, 200
 
 app.run(port=5000)
