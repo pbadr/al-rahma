4d59b5133c489b111541b3d45f5d75a800936772
Badr <badrkmc@gmail.com>
2023-11-10 11:16:36 +0500

    more styling changes

--- frontend/src/app/components/ChatInput.tsx
+++ frontend/src/app/components/ChatInput.tsx
@@ -21,19 +21,19 @@ export default function ChatInput({ prompt, isLoading, handlePromptChange, onSub
             autoComplete="off"
           />
           <button
-            className={`absolute right-0 bottom-0 mr-3 mb-2 opacity-50 ${prompt.length > 0 && 'bg-secondary opacity-100'} p-1 px-2 rounded-full transition-colors`}
+            className={`absolute right-0 bottom-0 mr-3 mb-2 opacity-50 ${prompt.length > 0 && 'bg-secondary !opacity-100'} p-1 px-2 rounded-full transition-colors`}
             disabled={isLoading || prompt.length === 0}
             type="submit"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
             </svg>
           </button>
         </div>
       </div>
       <div>
-        <p className="text-sm mt-3 opacity-70">
-          This application uses GPT-4 and the information it generates <span className="font-bold">can</span> be inaccurate.
+        <p className=" text-xs text-center mt-3 opacity-70">
+          Al-Rahma can <b>sometimes</b> generate inaccurate information. Please remember to cross-check your answers
         </p>
       </div>
     </form>
