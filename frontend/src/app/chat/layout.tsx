7996f8a73c4cb35951a567f6c3b6bc4416fc7cb5
Badr <badrkmc@gmail.com>
2023-11-11 15:12:57 +0500

    add cross button on sidebar toggled

--- frontend/src/app/chat/layout.tsx
+++ frontend/src/app/chat/layout.tsx
@@ -26,9 +26,16 @@ export default function ChatLayout({ children }: { children: React.ReactNode }) 
     <main className="flex flex-col">
       <nav className="navbar">
         <div onClick={toggleSidebar} className="hamburger">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
+          {
+            sidebarToggled ? 
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+            : 
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          }
         </div>
         <div className="logo">
           <Image
