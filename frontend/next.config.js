625e0748bb8bd6a4ecbecd017db66af0817591c7
Badr <badrkmc@gmail.com>
2023-11-13 20:46:32 +0500

    test backend server

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_ROUTE: 'http://localhost:5000'
+    API_ROUTE: 'https://alrahma-backend-865cad6b6a3a.herokuapp.com/'
   },
   reactStrictMode: false
 }
